640efde1ed3cbf8adc387de7e9e97ce48ef97913
shy <1533103845@qq.com>
2024-09-02 20:04:41 +0800

    spring-boot-quickstart 首日项目完成

--- spring-boot-quickstart/src/main/java/top/shy/springboot/quickstart/service/MeetingService.java
+++ spring-boot-quickstart/src/main/java/top/shy/springboot/quickstart/service/MeetingService.java
@@ -1,10 +1,11 @@
 package top.shy.springboot.quickstart.service;
 
+import org.springframework.stereotype.Service;
 import top.shy.springboot.quickstart.entity.Meeting;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Service
 public class MeetingService {
     private final List<Meeting> meetings = new ArrayList<>();
     public boolean issRoomAvailable(Meeting newMeeting){
--- spring-boot-quickstart/src/main/java/top/shy/springboot/quickstart/service/UserService.java
+++ spring-boot-quickstart/src/main/java/top/shy/springboot/quickstart/service/UserService.java
@@ -1,10 +1,11 @@
 package top.shy.springboot.quickstart.service;
 
+import org.springframework.stereotype.Service;
 import top.shy.springboot.quickstart.entity.User;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Service
 public class UserService {
     private final List<User> users = List.of(
             new User(1L,"张三",22)
